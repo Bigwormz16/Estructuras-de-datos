8dc8e56f06f2c93d7f0971bc578b09c269e974bc
Fabrizio <fabrizio.flores@ucsp.edu.pe>
2015-11-18 18:31:21 -0500

    Update stacks.cpp

--- stacks.cpp
+++ stacks.cpp
@@ -24,53 +24,51 @@ public:
 	stackType(const stackType<Type>& otherStack);
 	~stackType();
 private:
-	int maxStackSize; //variable to store the maximum stack size
-	int stackTop; //variable to point to the top of the stack
-	Type *list; //pointer to the array that holds the stack elements
+	int maxStackSize; 
+	int stackTop; 
+	Type *list;
 	void copyStack(const stackType<Type>& otherStack);
 };
 template <class Type>
 void stackType<Type>::initializeStack()
 {
 	stackTop = 0;
-}//end initializeStack
+}
 template <class Type>
 bool stackType<Type>::isEmptyStack() const
 {
 	return(stackTop == 0);
-}//end isEmptyStack
+}
 template <class Type>
 bool stackType<Type>::isFullStack() const
 {
 	return(stackTop == maxStackSize);
-} //end isFullStack
+} 
 template <class Type>
 void stackType<Type>::push(const Type& newItem)
 {
 	if (!isFullStack())
 	{
-		list[stackTop] = newItem; //add newItem at the top
-		stackTop++; //increment stackTop
+		list[stackTop] = newItem; 
+		stackTop++; 
 	}
 	else
 		cout << "Cannot add to a full stack." << endl;
-}//end push
+}
 template <class Type>
 Type stackType<Type>::top() const
 {
-	assert(stackTop != 0); //if stack is empty, terminate the
-	//program
-	return list[stackTop - 1]; //return the element of the stack
-	//indicated by stackTop - 1
-}//end top
+	assert(stackTop != 0); 
+	return list[stackTop - 1]; 
+}
 template <class Type>
 void stackType<Type>::pop()
 {
 	if (!isEmptyStack())
-		stackTop--; //decrement stackTop
+		stackTop--; 
 	else
 		cout << "Cannot remove from an empty stack." << endl;
-}//end pop
+}
 template <class Type>
 void stackType<Type>::copyStack(const stackType<Type>& otherStack)
 {
@@ -78,10 +76,9 @@ void stackType<Type>::copyStack(const stackType<Type>& otherStack)
 	maxStackSize = otherStack.maxStackSize;
 	stackTop = otherStack.stackTop;
 	list = new Type[maxStackSize];
-	//copy otherStack into this stack
 	for (int j = 0; j < stackTop; j++)
 		list[j] = otherStack.list[j];
-} //end copyStack
+}
 template <class Type>
 stackType<Type>::stackType(int stackSize)
 {
@@ -93,30 +90,26 @@ stackType<Type>::stackType(int stackSize)
 		maxStackSize = 100;
 	}
 	else
-		maxStackSize = stackSize; //set the stack size to
-	//the value specified by
-	//the parameter stackSize
-	stackTop = 0; //set stackTop to 0
-	list = new Type[maxStackSize]; //create the array to
-	//hold the stack elements
-}//end constructor
+		maxStackSize = stackSize; 
+	stackTop = 0; 
+	list = new Type[maxStackSize]; 
+}
 template <class Type>
-stackType<Type>::~stackType() //destructor
+stackType<Type>::~stackType() 
 {
-	delete [] list; //deallocate the memory occupied
-	//by the array
-}//end destructor
+	delete [] list; 
+}
 template <class Type>
 stackType<Type>::stackType(const stackType<Type>& otherStack)
 {
 	list = NULL;
 	copyStack(otherStack);
-}//end copy constructor
+}
 template <class Type>
 const stackType<Type>& stackType<Type>::operator=
 	(const stackType<Type>& otherStack)
 {
-	if (this != &otherStack) //avoid self-copy
+	if (this != &otherStack) 
 		copyStack(otherStack);
 	return *this;
-} //end operator=
+}
