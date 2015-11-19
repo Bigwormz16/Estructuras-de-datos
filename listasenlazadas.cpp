86b4bfbeac6cafd475b968f841e2d2845ab26be0
Fabrizio <fabrizio.flores@ucsp.edu.pe>
2015-11-18 23:44:40 -0500

    Update listasenlazadas.cpp

--- listasenlazadas.cpp
+++ listasenlazadas.cpp
@@ -1,9 +1,51 @@
+#include<iostream>
+#include<assert.h>
+using namespace std;
 template <class Type>
-class linkedListType
-{
+struct nodeType{
+	Type info;
+	nodeType<Type> *link;
+};
+template <class Type>
+class linkedListIterator{
 public:
-	const linkedListType<Type>& operator=
-		(const linkedListType<Type>&);
+	linkedListIterator();
+	linkedListIterator(nodeType<Type> *ptr);
+	Type operator*();
+	linkedListIterator<Type> operator++();
+	bool operator==(const linkedListIterator<Type>& right) const;
+	bool operator!=(const linkedListIterator<Type>& right) const;
+private:
+	nodeType<Type> *current;
+};
+template <class Type>
+linkedListIterator<Type>::linkedListIterator(){
+	current = NULL;
+}
+template <class Type>
+linkedListIterator<Type>::linkedListIterator(nodeType<Type> *ptr){
+	current = ptr;
+}
+template <class Type>
+Type linkedListIterator<Type>::operator*(){
+	return current->info;
+}
+template <class Type>
+linkedListIterator<Type> linkedListIterator<Type>::operator++(){
+	current = current->link;
+	return *this;
+}
+template <class Type>
+bool linkedListIterator<Type>::operator==(const linkedListIterator<Type>& right) const{
+	return (current == right.current);
+}
+template <class Type>
+bool linkedListIterator<Type>::operator!=(const linkedListIterator<Type>& right) const{
+	return (current != right.current);
+}
+template <class Type>
+class linkedListType{
+public:
 	void initializeList();
 	bool isEmptyList() const;
 	void print() const;
@@ -11,146 +53,195 @@ public:
 	void destroyList();
 	Type front() const;
 	Type back() const;
-	virtual bool search(const Type& searchItem) const = 0;
-	virtual void insertFirst(const Type& newItem) = 0;
-	virtual void insertLast(const Type& newItem) = 0;
-	virtual void deleteNode(const Type& deleteItem) = 0;
 	linkedListIterator<Type> begin();
 	linkedListIterator<Type> end();
 	linkedListType();
-	linkedListType(const linkedListType<Type>& otherList);
 	~linkedListType();
+	bool search(const Type& searchItem) const;
+	void insertFirst(const Type& newItem);
+	void insertLast(const Type& newItem);
+	void deleteNode(const Type& deleteItem);
 protected:
 	int count;
 	nodeType<Type> *first;
-	nodeType<Type> *last; 
-private:
-	void copyList(const linkedListType<Type>& otherList);
+	nodeType<Type> *last;
 };
-
 template <class Type>
-bool linkedListType<Type>::isEmptyList() const
-{
+bool linkedListType<Type>::isEmptyList() const{
 	return (first == NULL);
 }
 template <class Type>
-linkedListType<Type>::linkedListType()
-{
+linkedListType<Type>::linkedListType(){
 	first = NULL;
 	last = NULL;
 	count = 0;
 }
 template <class Type>
-void linkedListType<Type>::destroyList()
-{
+void linkedListType<Type>::destroyList(){
 	nodeType<Type> *temp;
-	while (first != NULL)
-	{
-		temp = first; 
+	while (first != NULL){
+		temp = first;
 		first = first->link;
 		delete temp;
 	}
-	last = NULL; 
+	last = NULL;
 	count = 0;
 }
 template <class Type>
-void linkedListType<Type>::initializeList()
-{
+void linkedListType<Type>::initializeList(){
 	destroyList(); 
 }
-void linkedListType<Type>::print() const
-{
+template <class Type>
+void linkedListType<Type>::print() const{
 	nodeType<Type> *current; 
 	current = first;
-	while (current != NULL) 
-	{
+	while (current != NULL) {
 		cout << current->info << " ";
 		current = current->link;
 	}
 }
 template <class Type>
-	int linkedListType<Type>::length() const
-{
+int linkedListType<Type>::length() const{
 	return count;
 }
 template <class Type>
-	Type linkedListType<Type>::front() const
-{
+Type linkedListType<Type>::front() const{
 	assert(first != NULL);
 	return first->info; 
 }
 template <class Type>
-	Type linkedListType<Type>::back() const
-{
+Type linkedListType<Type>::back() const{
 	assert(last != NULL);
-	return last->info; 
+	return last->info;
 }
 template <class Type>
-	linkedListIterator<Type> linkedListType<Type>::begin()
-{
+linkedListIterator<Type> linkedListType<Type>::begin(){
 	linkedListIterator<Type> temp(first);
 	return temp;
 }
 template <class Type>
-linkedListIterator<Type> linkedListType<Type>::end()
-{
+linkedListIterator<Type> linkedListType<Type>::end(){
 	linkedListIterator<Type> temp(NULL);
 	return temp;
 }
 template <class Type>
-void linkedListType<Type>::copyList
-	(const linkedListType<Type>& otherList)
-{
-	nodeType<Type> *newNode; 
+linkedListType<Type>::~linkedListType(){
+	destroyList();
+}
+template <class Type>
+bool linkedListType<Type>::search(const Type& searchItem) const{
 	nodeType<Type> *current;
-	if (first != NULL) 
-		destroyList();
-	if (otherList.first == NULL) 
-	{
-		first = NULL;
-		last = NULL;
-		count = 0;
+	bool found = false;
+	current = first;
+	while (current != NULL && !found)
+		if (current->info == searchItem)
+			found = true;
+		else
+			current = current->link;
+		return found;
+}
+template <class Type>
+void linkedListType<Type>::insertFirst(const Type& newItem){
+	nodeType<Type> *newNode; 
+	newNode = new nodeType<Type>; 
+	newNode->info = newItem;
+	newNode->link = first; 
+	first = newNode;
+	count++; 
+	if (last == NULL)
+		last = newNode;
+}
+template <class Type>
+void linkedListType<Type>::insertLast(const Type& newItem){
+	nodeType<Type> *newNode;
+	newNode = new nodeType<Type>;
+	newNode->info = newItem; 
+	newNode->link = NULL; 
+	if (first == NULL){
+		first = newNode;
+		last = newNode;
+		count++;
 	}
-	else
-	{
-		current = otherList.first; 
-		count = otherList.count;
-		first = new nodeType<Type>; 
-		first->info = current->info;
-		first->link = NULL;
-		last = first;
-		current = current->link;
-		while (current != NULL)
-		{
-			newNode = new nodeType<Type>; 
-			newNode->info = current->info; 
-			newNode->link = NULL; 
-			last->link = newNode; 
-			last = newNode; 
-			current = current->link; 
+	else{
+		last->link = newNode;
+		last = newNode; 
+		count++; 
+	}
+}
+template <class Type>
+void linkedListType<Type>::deleteNode(const Type& deleteItem){
+	nodeType<Type> *current; 
+	nodeType<Type> *trailCurrent; 
+	bool found;
+	if (first == NULL) 
+		cout << "Cannot delete from an empty list."
+		<< endl;
+	else{
+		if (first->info == deleteItem) {
+			current = first;
+			first = first->link;
+			count--;
+			if (first == NULL)
+				last = NULL;
+			delete current;
+		}
+		else {
+			found = false;
+			trailCurrent = first; 
+			current = first->link; 
+			while (current != NULL && !found){
+				if (current->info != deleteItem){
+					trailCurrent = current;
+					current = current-> link;
+				}else
+					found = true;
+			}
+			if (found){
+				trailCurrent->link = current->link;
+				count--;
+				if (last == current) 
+					last = trailCurrent; 
+				delete current;
+			}else
+				cout << "The item to be deleted is not in "
+				<< "the list." << endl;
 		}
 	}
 }
-template <class Type>
-linkedListType<Type>::~linkedListType()
-{
-	destroyList();
 
-}
 template <class Type>
-linkedListType<Type>::linkedListType
-	(const linkedListType<Type>& otherList)
-{
-	first = NULL;
-	copyList(otherList);
-}
-template <class Type>
-const linkedListType<Type>& linkedListType<Type>::operator=
-	(const linkedListType<Type>& otherList)
-{
-	if (this != &otherList)
-	{
-		copyList(otherList);
+linkedListType<Type> operator+(const linkedListType <Type>A , const linkedListType <Type>B){
+	linkedListType<Type>Temp;
+	Temp.initializeList();
+	for(int i=0;i<A.length();i++){
+		Temp.insertFirst(A.front());
+		A.deleteNode(A.front());
 	}
-	return *this;
+	for(int j=0;j<B.length();j++){
+		Temp.insertFirst(B.front());
+		B.deleteNode(B.front());
+	}
+	return Temp;
+}
+int main(){
+	linkedListType<int> lista;
+	lista.initializeList();
+	lista.insertFirst(10);
+	lista.insertFirst(102);
+	lista.insertFirst(101);
+	lista.print();
+	cout<<endl;
+	cout<<"numero de elemetos de la lista son :"<<lista.length()<<endl;
+	linkedListType<int> lista2;
+	lista2.initializeList();
+	lista2.insertFirst(104);
+	lista2.insertFirst(107);
+	lista2.insertFirst(666);
+	lista2.print();
+	cout<<endl;
+	cout<<"numero de elemetos de la lista son :"<<lista2.length()<<endl;
+	linkedListType<int> lista3;
+	lista3.initializeList();
+	lista3=lista+lista2;
+	return 0;
 }
