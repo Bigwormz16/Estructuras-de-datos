e7c537aa8e0eee765291a6a2709695ef558d2090
Fabrizio <fabrizio.flores@ucsp.edu.pe>
2015-11-18 22:39:22 -0500

    Update stacks.cpp

--- stacks.cpp
+++ stacks.cpp
@@ -1,115 +1,91 @@
+#include<iostream>
+#include <assert.h>
+using namespace std;
 template <class Type>
-class stackADT
-{
+class stackType{
 public:
-	virtual void initializeStack() = 0;
-	virtual bool isEmptyStack() const = 0;
-	virtual bool isFullStack() const = 0;
-	virtual void push(const Type& newItem) = 0;
-	virtual Type top() const = 0;
-	virtual void pop() = 0;
-};
-template <class Type>
-class stackType: public stackADT<Type>
-{
-public:
-	const stackType<Type>& operator=(const stackType<Type>&);
 	void initializeStack();
 	bool isEmptyStack() const;
 	bool isFullStack() const;
 	void push(const Type& newItem);
 	Type top() const;
 	void pop();
-	stackType(int stackSize = 100);
-	stackType(const stackType<Type>& otherStack);
+	stackType(int stackSize = 101);
 	~stackType();
-private:
-	int maxStackSize; 
-	int stackTop; 
 	Type *list;
-	void copyStack(const stackType<Type>& otherStack);
+private:
+	int maxStackSize;
+	int stackTop; 
 };
 template <class Type>
-void stackType<Type>::initializeStack()
-{
+void stackType<Type>::initializeStack(){
 	stackTop = 0;
 }
 template <class Type>
-bool stackType<Type>::isEmptyStack() const
-{
+bool stackType<Type>::isEmptyStack() const{
 	return(stackTop == 0);
 }
 template <class Type>
-bool stackType<Type>::isFullStack() const
-{
+bool stackType<Type>::isFullStack() const{
 	return(stackTop == maxStackSize);
-} 
+}
 template <class Type>
-void stackType<Type>::push(const Type& newItem)
-{
-	if (!isFullStack())
-	{
-		list[stackTop] = newItem; 
-		stackTop++; 
+void stackType<Type>::push(const Type& newItem){
+	if (!isFullStack()){
+		list[stackTop] = newItem; //add newItem at the top
+		stackTop++; //increment stackTop
 	}
-	else
-		cout << "Cannot add to a full stack." << endl;
+	else{cout << "No puede aumentar a una pila llena" << endl;}
 }
 template <class Type>
-Type stackType<Type>::top() const
-{
-	assert(stackTop != 0); 
-	return list[stackTop - 1]; 
+Type stackType<Type>::top() const{
+	assert(stackTop != 0);
+	return list[stackTop - 1];
 }
 template <class Type>
-void stackType<Type>::pop()
-{
+void stackType<Type>::pop(){
 	if (!isEmptyStack())
-		stackTop--; 
-	else
-		cout << "Cannot remove from an empty stack." << endl;
+		stackTop--;
+	else{cout << "Ya no puede borrar de una lista vacia" << endl;}
 }
 template <class Type>
-void stackType<Type>::copyStack(const stackType<Type>& otherStack)
-{
-	delete [] list;
-	maxStackSize = otherStack.maxStackSize;
-	stackTop = otherStack.stackTop;
-	list = new Type[maxStackSize];
-	for (int j = 0; j < stackTop; j++)
-		list[j] = otherStack.list[j];
-}
-template <class Type>
-stackType<Type>::stackType(int stackSize)
-{
-	if (stackSize <= 0)
-	{
-		cout << "Size of the array to hold the stack must "
-			<< "be positive." << endl;
-		cout << "Creating an array of size 100." << endl;
+stackType<Type>::stackType(int stackSize){
+	if (stackSize <= 0){
+		cout<<"En ves de 0 que le parece 100 y lo haremos asi"<<endl;
 		maxStackSize = 100;
 	}
 	else
-		maxStackSize = stackSize; 
-	stackTop = 0; 
-	list = new Type[maxStackSize]; 
+		maxStackSize = stackSize;
+	stackTop = 0;
+	list = new Type[maxStackSize];
 }
 template <class Type>
-stackType<Type>::~stackType() 
-{
-	delete [] list; 
+stackType<Type>::~stackType(){
+	delete [] list;
 }
-template <class Type>
-stackType<Type>::stackType(const stackType<Type>& otherStack)
-{
-	list = NULL;
-	copyStack(otherStack);
+template<class Type>
+stackType<Type> operator+(const stackType<Type>A , const stackType<Type>B){
+	stackType<Type>Temp(101);
+	for (int i=0;i<50;i++){
+		Temp.push(A.list[i]);
+	}
+	for(int j=0;j<50;j++){
+		Temp.push(B.list[j]);
+	}
+	return Temp;
 }
-template <class Type>
-const stackType<Type>& stackType<Type>::operator=
-	(const stackType<Type>& otherStack)
-{
-	if (this != &otherStack) 
-		copyStack(otherStack);
-	return *this;
+int main(){
+	stackType<int> pila(50);
+	pila.initializeStack();
+	pila.push(10);
+	cout<<pila.top()<<endl;
+	pila.pop();
+	pila.push(44);
+	cout<<pila.top()<<endl;
+	stackType<int> pila2(50);
+	pila2.initializeStack();
+	pila2.push(10);
+	stackType<int> pila3(100);
+	pila3=pila+pila2;
+	return 0;
 }
