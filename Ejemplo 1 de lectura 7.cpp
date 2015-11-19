9f10e726f6309a982a331c887ea66e05cb692bf7
Fabrizio <fabrizio.flores@ucsp.edu.pe>
2015-11-19 00:20:19 -0500

    Create Ejemplo 1 de lectura 7.cpp

---
+++ Ejemplo 1 de lectura 7.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+using namespace std;
+class Vehicle {
+	protected : 
+		string license ; 
+		int year ;
+		string year1;
+	public :
+		string stringify(int year){
+			year1=static_cast<ostringstream*>(&(ostringstream() << year))->str();
+			return year1;
+		}
+		Vehicle( const string &myLicense, const int myYear): license(myLicense), year(myYear) {}
+		const string getDesc() {
+			return license + " from " + stringify(&year);
+		}
+		const string &getLicense() const {
+			return license;
+		} 
+		const int getYear() const {
+			return year;
+		} 
+		
+};
+class Car : public Vehicle { // Makes Car inherit from Vehicle 
+	string style;
+public :
+	Car( const string &myLicense, const int myYear, const string &myStyle) : Vehicle(myLicense, myYear), style(myStyle) {}
+	const string &getStyle() {return style;}
+};
